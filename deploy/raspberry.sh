7d60951a61629f014a9380b8ad3dc8b1e603d29d
raspsang <sang.chen@outlook.com>
2020-08-23 17:38:29 +0100

    update

--- deploy/raspberry.sh
+++ deploy/raspberry.sh
@@ -1,28 +1,8 @@
 #!/bin/bash
 
-# var_conf
-GIT_EMAIL="sang.chen@outlook.com"
-GIT_NAME="raspsang"
-SCPP=~/project/script
-SCPP_BA=$SCPP/runcommand/.bash_aliases
-SCPP_BR=$SCPP/runcommand/.bashrc
-SCPP_VIM=$SCPP/runcommand/.vimrc
-SCPP_V2=$SCPP/v2ray
+# --------------------------------------------- common ---------------------------------------------
 
-init_source_tsinghua() {
-    sudo echo "deb http://mirrors.tuna.tsinghua.edu.cn/raspberry-pi-os/raspbian/ buster main non-free contrib rpi" \
-        > /etc/apt/sources.list
-    sudo echo "deb-src http://mirrors.tuna.tsinghua.edu.cn/raspberry-pi-os/raspbian/ buster main non-free contrib rpi" \
-        >> /etc/apt/sources.list
-    sudo echo "deb http://mirrors.tuna.tsinghua.edu.cn/raspberrypi/ buster main ui" \
-        > /etc/apt/sources.list.d/raspi.list
-}
-
-init_apt() {
-    sudo apt-get update 
-    sudo apt-get upgrade -y
-    sudo rm /etc/systemd/network/99-default.link -f
-    
+reboot_ask() {
     echo "System should be reboot, are you sure you want to reboot? (yes/no)"
     read Arg
     case $Arg in
@@ -35,49 +15,174 @@ init_apt() {
     esac
 }
 
-install_base() {
-    sudo apt-get install -y \
-        zip unzip \
-        wget net-tools \
-        gcc gcc-c++ \
-        python python3 python-pip python3-pip
+# --------------------------------------------- init ---------------------------------------------
+
+init() {
+    echo "deb http://mirrors.tuna.tsinghua.edu.cn/raspberry-pi-os/raspbian/ buster main non-free contrib rpi" \
+        > /etc/apt/sources.list
+    echo "deb-src http://mirrors.tuna.tsinghua.edu.cn/raspberry-pi-os/raspbian/ buster main non-free contrib rpi" \
+        >> /etc/apt/sources.list
+    echo "deb http://mirrors.tuna.tsinghua.edu.cn/raspberrypi/ buster main ui" \
+        > /etc/apt/sources.list.d/raspi.list
+
+    apt-get update 
+    apt-get upgrade -y
 }
 
-install_git() {
-    sudo apt-get install git -y
+install_bases() {
+    apt-get install -y \
+        zip unzip \
+        wget net-tools curl \
+        gcc gcc-c++ \
+        python python3 python-pip python3-pip \
+        git vim
+}
+
+install_docker() {
+    # get-docker
+    curl -fsSL http://get.docker.com -o get-docker.sh
+    sh get-docker.sh --mirror Aliyun
+
+    # set aliyun mirrors
+    echo "{ \"registry-mirrors\": [\"https://57qfh1yo.mirror.aliyuncs.com\"] }" > /etc/docker/daemon.json
+    systemctl daemon-reload
+    systemctl restart docker
+}
+
+install_v2ray() {
+    curl -O https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh
+    curl -O https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-dat-release.sh
+    bash install-release.sh
+    bash install-dat-release.sh
+}
+
+# --------------------------------------------- docker ---------------------------------------------
+
+install_samba() {
+    docker pull dperson/samba
+    docker run --name samba \
+        -p 139:139 -p 445:445 \
+        -v /mnt:/mnt \
+        -d dperson/samba \
+        -u "pi;pi" -u "guest;guest" \
+        -s "nas;/mnt/nas;yes;yes;no;pi,guest;pi;pi;" \
+        -s "secret;/mnt/secret;yes;no;no;pi;pi;pi"
+}
+
+install_h5ai() {
+    docker pull clue/h5ai
+    docker run --name h5ai \
+        -p 80:80 \
+        -v /data/h5ai:/var/www -v /mnt:/mnt \
+        -d clue/h5ai
+}
+
+install_nextcloud() {
+    docker pull postgres
+    docker pull nextcloud
+    docker run --name postgres \ 
+        --restart=always \
+        -p 5432:5432 \
+        -v /data/postgresql:/var/lib/postgresql/data \
+        -e POSTGRES_PASSWORD=postgres \
+        -d postgres
+    docker run --name nextcloud \
+        --restart=always \
+        -p 8080:80 \
+        -v /data/nextcloud:/var/www/html \
+        -v /mnt:/mnt \
+        --link postgres:postgres \
+        nextcloud
+}
+
+# --------------------------------------------- configure ---------------------------------------------
+
+GIT_EMAIL="sang.chen@outlook.com"
+GIT_NAME="raspsang"
+SCPP=~/project/script
+SCPP_BASH=$SCPP/runcommand/.bash_aliases
+SCPP_VIM=$SCPP/runcommand/.vimrc
+SCPP_V2=$SCPP/v2ray
+SCPP_SMB=$SCPP/samba/smb.conf
+
+config_clone() {
+    if [ -d "$SCPP" ]; then
+        git clone http://github.com/gitsang/script.git $SCPP
+    fi
+}
+
+config_v2ray() {
+    unzip $SCPP_V2/config.zip
+    cp $SCPP_V2/config.json /etc/v2ray/config.json
+    systemctl restart v2ray
+    systemctl status v2ray
+    #export {http,https}_proxy=http://127.0.0.1:1080
+}
+
+config_mount() {
+    echo "/dev/sda1 /mnt/nas    ntfs-3g defaults,noexec,umask=0000 0 0" >> /etc/fstab
+    echo "/dev/sda2 /mnt/secret ntfs-3g defaults,noexec,umask=0000 0 0" >> /etc/fstab
+}
+
+config_samba() {
+    cp $SCPP_SMB /etc/samba/smb.conf
+    systemctl restart smbd
+    systemctl status smbd
+}
+
+config_git() {
     git config --global user.email $GIT_EMAIL
     git config --global user.name $GIT_NAME
 }
 
-init_script() {
-    if [ -d "$SCPP" ]; then
-        git clone http://github.com/gitsang/script.git $SCPP
-    else
-        cd $SCPP && git pull
-    fi
-}
-
-init_bashrc() {
-    cp SCPP_BA ~/ # && cp SCPP_BR ~/
+config_bash() {
+    cp $SCPP_BASH ~/
     source ~/.bashrc
 }
 
-install_vim() {
-    sudo apt-get install vim -y
+config_vim() {
     cp $SCPP_VIM ~/
+    vim
 }
 
-install_OMV() {
-    #wget -O - https://github.com/OpenMediaVault-Plugin-Developers/installScript/raw/master/install | sudo bash
-    bash install-OpenMediaVault.sh
+config_all() {
+    config_clone
+    config_v2ray
+    config_mount
+    config_samba
+    config_git
+    config_bash
+    config_vim
 }
 
-install_v2ray() {
-    cd $SCPP_V2
-    unzip config.zip
-    bash v2ray.sh
-    cp config.json /etc/v2ray/
-}
+# --------------------------------------------- option ---------------------------------------------
+
+case "$1" in
+    "-h"|"--help")
+        echo "uasge:"
+        echo "    $0 [option] [param]"
+        exit;;
+    "--auto")
+        set -e
+        # init
+        init
+        install_bases
+        install_docker
+        install_v2ray
+
+        # config
+        config_all
+
+        # docker
+        install_samba
+        install_nextcloud
+        exit;;
+    *)
+        $0 --help
+        exit;;
+esac
+
+# --------------------------------------------- other ---------------------------------------------
 
 init_firewalld() {
     systemctl start firewalld
@@ -87,22 +192,3 @@ init_firewalld() {
     firewall-cmd --zone=public --list-ports
 }
 
-install_h5ai() {
-    echo "install_h5ai"
-}
-
-install_docker() {
-    # get-docker
-    curl -fsSL http://get.docker.com -o get-docker.sh
-    sudo sh get-docker.sh --mirror Aliyun
-
-    # set aliyun mirrors
-    echo "{ \"registry-mirrors\": [\"https://???.mirror.aliyuncs.com\"] }" > /etc/docker/daemon.json
-    sudo systemctl daemon-reload
-    sudo systemctl restart docker
-}
-
-mount_disk() {
-    sudo echo "/dev/sda1 /mnt/nas    ntfs-3g defaults,noexec,umask=0000 0 0" >> /etc/fstab
-    sudo echo "/dev/sda2 /mnt/secret ntfs-3g defaults,noexec,umask=0000 0 0" >> /etc/fstab
-}
