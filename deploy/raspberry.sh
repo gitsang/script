5cb15811b174206fd8377129174d9407f2d14f1c
raspsang <sang.chen@outlook.com>
2020-08-24 07:31:48 +0100

    update

--- deploy/raspberry.sh
+++ deploy/raspberry.sh
@@ -39,11 +39,17 @@ install_bases() {
 }
 
 install_docker() {
+    # remove old
+    sudo apt-get -y remove docker docker-engine docker.io containerd runc
+    sudo rm -fr /var/lib/docker/
+    sudo rm -fr /var/lib/containerd/
+    sudo apt -y autoremove
+
     # option
     apt-get --yes --no-install-recommends --reinstall install lsb-release
-    #apt-get --yes --no-install-recommends install dirmngr gnupg
-    #apt-get --yes --no-install-recommends install monit postfix
-    #apt-get --yes --fix-broken install
+    apt-get --yes --no-install-recommends install dirmngr gnupg
+    apt-get --yes --no-install-recommends install monit postfix
+    apt-get --yes --fix-broken install
 
     # get-docker
     curl -fsSL http://get.docker.com -o get-docker.sh
