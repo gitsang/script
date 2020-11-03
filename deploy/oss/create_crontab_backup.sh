0bc3ef36b0a67c15a4244295b52378916ea420b3
gitsang <sang.chen@outlook.com>
2020-11-03 13:56:42 +0800

    auto commit

--- deploy/oss/create_crontab_backup.sh
+++ deploy/oss/create_crontab_backup.sh
@@ -1,19 +1,19 @@
 
-echo "" > /etc/cron.daily/oss-backup
-chmod +x /etc/cron.daily/oss-backup
-echo "" > /var/log/oss-backup
+INTERVAL=weekly
+SHNAME=oss-backup
+CRON_PATH=/etc/cron.${INTERVAL}/${SHNAME}
+LOG_PATH=/var/log/oss-backup
+
+> ${CRON_PATH}
+> ${LOG_PATH}
+chmod +x ${CRON_PATH}
 
 echo "date >> /var/log/oss-backup" \
-    >> /etc/cron.daily/oss-backup
-echo "/usr/local/bin/ossutil64 ls oss:// >> /var/log/oss-backup" \
-    >> /etc/cron.daily/oss-backup
+    >> ${CRON_PATH}
 echo "/usr/local/bin/ossutil64 cp -r /share/package oss://sang-repo/package/ -u >> /var/log/oss-backup" \
-    >> /etc/cron.daily/oss-backup
+    >> ${CRON_PATH}
 echo "/usr/local/bin/ossutil64 cp -r /share/project oss://sang-repo/project/ -u >> /var/log/oss-backup" \
-    >> /etc/cron.daily/oss-backup
+    >> ${CRON_PATH}
 echo "/usr/local/bin/ossutil64 cp -r /share/books oss://sang-repo/books/ -u >> /var/log/oss-backup" \
->> /etc/cron.daily/oss-backup
-
-echo "0 6 * * * root /etc/cron.daily/oss-backup" > /etc/cron.d/oss-backup
-echo "Create crontab finished! Will auto backup at 06:00am"
+    >> ${CRON_PATH}
 
