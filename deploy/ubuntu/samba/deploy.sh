5c7ab878eb83e9d382ab4873c380a9c1c5d1e586
gitsang <sang.chen@outlook.com>
2020-11-20 03:08:57 +0100

    fix

--- deploy/ubuntu/samba/deploy.sh
+++ deploy/ubuntu/samba/deploy.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+
 apt install samba -y
 
 cp smb.conf /etc/samba/smb.conf
 
 smbpasswd -a root
 
-systemctl enable smb
-systemctl restart smb
+systemctl enable smbd
+systemctl restart smbd
+
