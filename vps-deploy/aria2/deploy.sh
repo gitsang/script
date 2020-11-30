4eaa0632f701e674521ca2e9aa3cf1d6d884364d
gitsang <sang.chen@outlook.com>
2020-11-30 08:28:21 +0100

    auto commit

--- vps-deploy/aria2/deploy.sh
+++ vps-deploy/aria2/deploy.sh
@@ -13,24 +13,18 @@ aria2() {
 
     # webui
     cd /etc/aria2 && git clone https://github.com/ziahamza/webui-aria2.git
+    cd -
 
     # systemd
     cp aria2.service /usr/lib/systemd/system/
     systemctl enable aria2
     systemctl start aria2
     systemctl status aria2
+
     cp aria2-webui.service /usr/lib/systemd/system/
     systemctl enable aria2-webui
     systemctl start aria2-webui
     systemctl status aria2-webui
-
-    # apache2
-    grep -q -e "Listen 80" /etc/apache2/ports.conf || echo "Listen 80" >> /etc/apache2/ports.conf
-    cp ../apache2/aria2.conf /etc/apache2/sites-available/aria2.conf
-    a2ensite aria2
-    a2enmod proxy_http
-    apache2ctl configtest
-    systemctl restart apache2
 }
 
 aria2
