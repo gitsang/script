51e2fb33c0a2a02871ad46277dafb5d85ab41ba9
gitsang <sang.chen@outlook.com>
2021-01-13 11:53:12 +0800

    update fix.sh

--- config/bash/fix.sh
+++ config/bash/fix.sh
@@ -3,10 +3,9 @@ FILES=`ls`
 
 for F in $FILES
 do
-    if [ "$F" == "run.sh" ]; then
+    if [ "$F" == "fix.sh" ]; then
         continue
     fi
-
     T=`echo $F | awk -F'-%trash%-' '{print $1}' | sed 's/\://g' | sed 's/-//'| sed 's/-//'`
     P=`echo $F | awk -F'-%trash%-' '{print $2}' | sed 's/\^\^/##/g'`
     N=$T-%TRASH%-$P
