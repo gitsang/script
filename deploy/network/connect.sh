69fa29426c00e40db514543c59f241a34a54ede0
gitsang <sang.chen@outlook.com>
2021-02-04 01:51:14 +0000

    update network dpl

--- deploy/network/connect.sh
+++ deploy/network/connect.sh
@@ -2,7 +2,10 @@ ifconfig -a
 ifconfig wlp2s0 up
 wpa_passPhrase CSTH-AX_5G CSTH636797 > /etc/wpa_supplicant/CSTH-AX_5G.conf
 wpa_supplicant -i wlp2s0 -c /etc/wpa_supplicant/CSTH-AX_5G.conf -B
+#wpa_passPhrase CSTH-AX_5G CSTH636797 > /etc/wpa_supplicant/wpa_supplicant-wlp2s0.conf
+#wpa_supplicant -i wlp2s0 -c /etc/wpa_supplicant/wpa_supplicant-wlp2s0.conf -B
 dhclient wlp2s0
 ifconfig
 iwconfig
+
 
---
+++ deploy/network/netplan.sh
@@ -0,0 +1,14 @@
+
+cat > /etc/netplan/00-installer-config.yaml << EOF
+network:
+  ethernets:
+    enp1s0:
+      addresses: [192.168.5.100/24]
+      gateway4: 192.168.5.1
+    wlp2s0:
+      addresses: [192.168.5.120/24]
+      gateway4: 192.168.5.1
+  version: 2
+EOF
+
+netplan apply
