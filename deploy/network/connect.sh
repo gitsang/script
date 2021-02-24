4207ee069d00cee47f692ae0b9e956bc7b8e117f
gitsang <sang.chen@outlook.com>
2021-02-24 09:12:52 +0000

    update network

--- deploy/network/connect.sh
+++ deploy/network/connect.sh
@@ -1,11 +1,15 @@
+#!/bin/bash
+
 ifconfig -a
 ifconfig wlp2s0 up
-wpa_passPhrase CSTH-AX_5G CSTH636797 > /etc/wpa_supplicant/CSTH-AX_5G.conf
-wpa_supplicant -i wlp2s0 -c /etc/wpa_supplicant/CSTH-AX_5G.conf -B
-#wpa_passPhrase CSTH-AX_5G CSTH636797 > /etc/wpa_supplicant/wpa_supplicant-wlp2s0.conf
-#wpa_supplicant -i wlp2s0 -c /etc/wpa_supplicant/wpa_supplicant-wlp2s0.conf -B
-dhclient wlp2s0
+wpa_passPhrase CSTH-AX_5G CSTH636797 > /etc/wpa_supplicant/wpa_supplicant-wlp2s0.conf
+systemctl restart wpa_supplicant@wlp2s0.service
+systemctl status wpa_supplicant@wlp2s0.service
 ifconfig
 iwconfig
 
+#wpa_passPhrase CSTH-AX_5G CSTH636797 > /etc/wpa_supplicant/CSTH-AX_5G.conf
+#wpa_supplicant -i wlp2s0 -c /etc/wpa_supplicant/CSTH-AX_5G.conf -B
+#wpa_supplicant -i wlp2s0 -c /etc/wpa_supplicant/wpa_supplicant-wlp2s0.conf -B
+#dhclient wlp2s0
 
