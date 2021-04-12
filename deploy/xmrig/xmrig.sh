764971fab175db7a8213f4eeb0439573aa971ff2
yl2356 <yl2356@yealink.com>
2021-04-12 14:32:44 +0800

    update xmrig

--- deploy/xmrig/xmrig.sh
+++ deploy/xmrig/xmrig.sh
@@ -1,26 +1,79 @@
 #!/bin/bash
 
-TGZ=xmrig-6.11.1-linux-x64.tar.gz
-if [ ! -f "${TGZ}" ]; then
-    wget https://github.com/xmrig/xmrig/releases/download/v6.11.1/xmrig-6.11.1-linux-x64.tar.gz
-fi
-
-DIR=xmrig-6.11.1
-if [ ! -d "${DIR}" ]; then
-    tar zxvf ${TGZ}
-fi
-
 BIN_PATH=/usr/local/bin/
-if [ ! -f "${BIN_PATH}/xmrig" ]; then
-    cp ${DIR}/xmrig ${BIN_PATH}
-fi
-
 CONF_PATH=/usr/local/etc/xmrig/
 SERVICE_PATH=/etc/systemd/system/
-mkdir -p ${CONF_PATH}
-cp ./config.json ${CONF_PATH}
-cp ./xmrig.service ${SERVICE_PATH}
 
-systemctl daemon-reload
-systemctl restart xmrig.service
-systemctl status xmrig.service
+download_xmrig() {
+    mkdir xmrig-C3
+    cd xmrig-C3
+    wget https://github.com/C3Pool/xmrig-C3/releases/download/v6.11.0-C3/xmrig-v6.11.0-C3-linux-Static.tar.gz
+    tar zxvf xmrig-v6.11.0-C3-linux-Static.tar.gz
+    cp ./xmrig ${BIN_PATH}
+}
+
+build_xmrig() {
+    git clone https://github.com/C3Pool/xmrig-C3.git
+    cd xmrig-C3
+    mkdir build
+    cd build
+    cmake .. -DWITH_HWLOC=OFF
+    make -j16
+    cp ./xmrig ${BIN_PATH}
+}
+
+init_xmrig() {
+    mkdir -p ${CONF_PATH}
+    cp ./config.json ${CONF_PATH}
+    cp ./xmrig.service ${SERVICE_PATH}
+}
+
+start_xmrig() {
+    systemctl daemon-reload
+    systemctl restart xmrig.service
+    systemctl status xmrig.service
+}
+
+run_xmrig() {
+    ./xmrig -c ./config.json
+}
+
+help_xmrig() {
+    echo "Usage:"
+    echo "    ./xmrig.sh [option]"
+    echo "option:"
+    echo "    -h help"
+    echo "    -d download_xmrig"
+    echo "    -b build_xmrig"
+    echo "    -i init_xmrig"
+    echo "    -s start_xmrig"
+    echo "    -r run_xmrig"
+}
+
+OPT=$1
+case $OPT in
+    "-h")
+        help_xmrig
+        ;;
+    "-d")
+        download_xmrig
+        ;;
+    "-b")
+        build_xmrig
+        ;;
+    "-i")
+        init_xmrig
+        ;;
+    "-s")
+        start_xmrig
+        ;;
+    "-r")
+        run_xmrig
+        ;;
+    *)
+        download_xmrig
+        init_xmrig
+        start_xmring
+        ;;
+esac
+
