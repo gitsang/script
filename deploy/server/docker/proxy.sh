3c6dbbf3844316191d8edda4dbf8fb49c06e72b0
gitsang <sang.chen@outlook.com>
2021-06-22 15:27:27 +0800

    deploy(docker): add runtime proxy

--- deploy/server/docker/proxy.sh
+++ deploy/server/docker/proxy.sh
@@ -9,20 +9,6 @@ Environment="HTTPS_PROXY=http://127.0.0.1:1081/"
 Environment="NO_PROXY=localhost,127.0.0.1,.example.com"
 EOF
 
-# docker runtime proxy
-mkdir -p ~/.docker
-tee ~/.docker/config.json <<-'EOF'
-{
-  "proxies": {
-    "default": {
-      "httpProxy": "http://127.0.0.1:1081",
-      "httpsProxy": "http://127.0.0.1:1081",
-      "noProxy": "localhost,127.0.0.1,.example.com"
-    }
-  }
-}
-EOF
-
 # reload
 systemctl daemon-reload
 systemctl restart docker
