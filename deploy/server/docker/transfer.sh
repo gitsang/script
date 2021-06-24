4dd68bb3edf5b242442e6fbf6eac007dbd71f5f9
gitsang <sang.chen@outlook.com>
2021-06-24 14:34:09 +0800

    deploy(html): update html port

--- deploy/server/docker/transfer.sh
+++ deploy/server/docker/transfer.sh
@@ -2,7 +2,7 @@ docker rm -f transfer
 docker run -d \
     --name transfer \
     --restart=always \
-    -p 8080:8080 \
+    -p 8082:8080 \
     -v /share/transit/:/transit/ \
     dutchcoders/transfer.sh \
     --provider local \
